34f24e5b247515d841723686f9ba37410fef21e6
Tim-rdt <troudot.42@gmail.com>
2022-11-25 00:05:35 +0100

    push

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,52 @@
 /*   By: troudot <troudot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 17:36:55 by troudot           #+#    #+#             */
-/*   Updated: 2022/11/22 17:37:52 by troudot          ###   ########.fr       */
+/*   Updated: 2022/11/25 00:05:07 by troudot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	check_if_return(char *str)
+{
+	while (*str)
+		if (*str == '\0')
+			return (1);
+	return (0);
+}
+
+// return the actual line
+char	*get_line(int fd)
+{
+	static char	*str;
+	char		*str2;
+	int			tmp;
+
+	read(fd, str, BUFFER_SIZE);
+	tmp = check_if_return(str);
+	if (!tmp)
+	{
+		read(fd, str2, BUFFER_SIZE);
+		str = ft_strjoin(str, str2);
+	}
+	str = malloc(sizeof(char *) * check_if_return(str));
+	if (!str)
+		return (NULL);
+	return (str);
+}
+
+//go to the next line
+char	*get_next_line(int fd)
+{
+	
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,20 @@
 /*   By: troudot <troudot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 10:06:18 by troudot           #+#    #+#             */
-/*   Updated: 2022/11/22 16:50:00 by troudot          ###   ########.fr       */
+/*   Updated: 2022/11/24 23:59:05 by troudot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-	char	*get_next_line(int fd);
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
+
+char	*get_next_line(int fd);
 
 #endif
